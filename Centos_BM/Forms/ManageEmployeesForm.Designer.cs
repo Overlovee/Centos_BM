21e19d2aaf258495db0328bd9cb6a6b4d8d078ae
I'mFoodboy <vnn159357@gmail.com>
2023-12-12 20:59:54 +0700

    update

--- Centos_BM/Forms/ManageEmployeesForm.Designer.cs
+++ Centos_BM/Forms/ManageEmployeesForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Centos_BM.Forms
 {
-    partial class Home
+    partial class ManageEmployeesForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Home
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1212, 642);
-            this.Name = "Home";
-            this.Text = "Home";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "ManageEmployeesForm";
         }
 
         #endregion
